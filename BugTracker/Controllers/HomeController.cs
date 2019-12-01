e73285ba2257289dc69bdabf0dc126ab3542800e
camadoz <madozc@gmail.com>
2019-12-01 00:13:25 -0500

    Added line graph to dashboard page.

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace BugTracker.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
 
         private ApplicationSignInManager _signInManager;
 
@@ -117,7 +118,48 @@ namespace BugTracker.Controllers
             return Json(dataSet);
         }
 
-        
+
+
+        public long GetJavascriptTimestamp(DateTime input)
+        {
+            TimeSpan span = new TimeSpan(DateTime.Parse("1/1/1970").Ticks);
+            DateTime time = input.Subtract(span);
+            return (long)(time.Ticks / 10000);
+        }
+
+
+
+        public JsonResult GetTicketsPerDay()
+        {
+            var dataSet = new List<TicketsPerDay>();
+           
+            DateTime cutOfDate = DateTime.Now.AddDays(-30);
+            DateTime currDate;
+            var count = 0;
+
+            var tickets = db.Tickets.Where(t => t.Created > cutOfDate).ToList();
+            while (cutOfDate <= DateTime.Now)
+            {
+                currDate = cutOfDate.Date;
+                count = 0;
+                foreach (var ticket in tickets)
+                { 
+                    if(DateTime.Compare(ticket.Created.Date,currDate) == 0)
+                    {
+                        count++;
+                    }
+                
+                }
+
+                dataSet.Add(new TicketsPerDay { DayTimeOccurence = GetJavascriptTimestamp(currDate), occurences = count });
+                cutOfDate = cutOfDate.AddDays(1);
+            }
+
+            return Json(dataSet);
+        }
+
+
+
         public ActionResult Dashboard()
         {
 
---
+++ BugTracker/Models/TicketsPerDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class TicketsPerDay
+    {
+        public long DayTimeOccurence { get; set; }
+        public int occurences { get; set; }
+    }
+}
